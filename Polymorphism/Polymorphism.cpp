a8ed68cd5356376c71165ef5a082768739aac6d8
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-05-31 17:11:09 +0700

    membuat class seseorang

---
+++ Polymorphism/Polymorphism.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class seseorang {
+	virtual void pesan() = 0;
+	//virtual void pesan(){
+	    //cout << "Pesan dari seseorang" << endl;
+	//}
+	//virtual void cetak() = 0;
+	virtual void cetak() {
+
+	}
+};
