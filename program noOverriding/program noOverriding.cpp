11cd7eefa3376e5a4f150686dc8b612a1943e982
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-05-31 17:51:53 +0700

    memasukkan ke program utama

--- program noOverriding/program noOverriding.cpp
+++ program noOverriding/program noOverriding.cpp
@@ -7,3 +7,17 @@ public:
 		cout << "Hallo saya function dari base class";
 	}
 };
+
+class derivedClass : public baseClass {
+public:
+	void perkenalan(){
+		cout << "Hallo saya function dari derived class";
+	}
+};
+
+int main() {
+	derivedClass a;
+	a.perkenalan();
+
+	return 0;
+}
