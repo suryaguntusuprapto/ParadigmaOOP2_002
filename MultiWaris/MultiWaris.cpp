ab3e025285c26553debdb1d0daba650122bd3851
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-05-31 18:27:17 +0700

    membuat class pelajar dari pewarisan manusia dan orang

--- MultiWaris/MultiWaris.cpp
+++ MultiWaris/MultiWaris.cpp
@@ -27,3 +27,22 @@ public:
 		cout << "Manusia dihapus\n" << endl;
 	}
 };
+
+class pelajar : private manusia, public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pJenisKelamin, string pSekolah) :
+		orang(pNama),
+		manusia(pJenisKelamin),
+		sekolah(pSekolah) {
+		cout << "Pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "Pelajar dihapus\n" << endl;
+	}
+
+	string perkenalan() {
+		return "Hallo, nama saya " + nama + "dengan jenis kelamin" + jeniskelamin + "dari sekolah" + sekolah + "\n\n";
+	}
+};
