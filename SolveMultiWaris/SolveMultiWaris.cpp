b38e4f716455663b2148f650e7e25c0cf6f1c39f
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-05-31 17:33:13 +0700

    membuat class pelajar dengan pewarisan dari orang

--- SolveMultiWaris/SolveMultiWaris.cpp
+++ SolveMultiWaris/SolveMultiWaris.cpp
@@ -21,3 +21,13 @@ public:
 		cout << "pekerja dibuat\n" << endl;
 	}
 };
+
+class pelajar : virtual public orang {
+public:
+
+	pelajar(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "Pelajar dibuat\n" << endl;
+	}
+};
